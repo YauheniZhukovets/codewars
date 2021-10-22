61cbaa8035f384affa08bc6f5560c3c11f2b6e5f
YauheniZhukovets <92811758+YauheniZhukovets@users.noreply.github.com>
2021-10-22 22:02:33 +0300

    Grasshopper - Personalized Message

---
+++ 13PersonalizedMessage.js
@@ -0,0 +1,12 @@
+function greet (name, owner) {
+    if (name === owner) {
+        return "Hello boss"
+    } else {
+        return "Hello guest"
+    }
+};
+        
+
+var fu = greet("dick","dick");
+console.log(fu);
+
