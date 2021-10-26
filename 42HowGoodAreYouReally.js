8f1c7773f3faa12e7cf0067272731048ef4ed521
YauheniZhukovets <92811758+YauheniZhukovets@users.noreply.github.com>
2021-10-26 23:17:19 +0300

    How good are you really?

---
+++ 42HowGoodAreYouReally.js
@@ -0,0 +1,21 @@
+function betterThanAverage(classPoints, yourPoints) {
+    let allPoint = 0;
+    
+    for(i=0; i<classPoints.length; i++) {
+      allPoint += classPoints[i];
+    }
+
+    let midlePoint = allPoint/classPoints.length;
+    
+    if (yourPoints>midlePoint) {
+        return true
+    } else {
+        return false
+    }
+}
+
+
+let allPoints = [90, 40, 34, 57, 29, 72, 57, 88];
+let MyPoint = 10;
+let result = betterThanAverage(allPoints,MyPoint);
+console.log(result);
