baed25da224af8c046d9fa2cbb0f56cd9e209a8b
YauheniZhukovets <92811758+YauheniZhukovets@users.noreply.github.com>
2022-02-01 11:18:10 +0300

    64 Pillars

--- 64Pillars.js
+++ 64Pillars.js
@@ -1,4 +1,4 @@
-function pillars(num_pill, dist, width) {
+/*function pillars(num_pill, dist, width) {
      let distance =  dist * 100
     if (num_pill > 2) {
         let widthPills = (num_pill - 2) * width
@@ -6,6 +6,11 @@ function pillars(num_pill, dist, width) {
     } else if (num_pill === 2) {
         return distance
     } else return 0
-}
+}*/
 
 console.log(pillars(11,15,30))
+
+
+function pillars(num_pill, dist, width) {
+    return num_pill>1?(num_pill-1)*dist*100+(num_pill-2)*width:0;
+}
