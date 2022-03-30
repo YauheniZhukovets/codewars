45013c91855e12e8a60a6ce587c1fd9378f9280e
YauheniZhukovets <92811758+YauheniZhukovets@users.noreply.github.com>
2022-03-30 21:46:14 +0300

    Gravity Flip

--- 76.MergeTwoSortedArraysIntoOne.js
+++ 76.MergeTwoSortedArraysIntoOne.js
@@ -1,9 +1,22 @@
-function mergeArrays(arr1, arr2) {
-    let arraySorted = arr1.concat(arr2).sort((a,b) => a-b )
+// function mergeArrays(arr1, arr2) {
+//     let arraySorted = arr1.concat(arr2).sort((a,b) => a-b )
+//
+//  return [...new Set(arraySorted)]
+// }
+//
+// console.log(mergeArrays([1,3,5,7,9,11,12], [1,2,3,4,5,10,12]))
 
- return [...new Set(arraySorted)]
+
+function mergeArrays1(arr1, arr2) {
+    let arraySorted = arr1.concat(arr2).sort((a, b) => a - b)
+
+    return arraySorted.filter((item, index) => {
+        console.log(item,
+            index,
+            arraySorted.indexOf(item),
+            arraySorted.indexOf(item) === index)
+        return arraySorted.indexOf(item) === index
+    })
 }
 
-console.log(mergeArrays([1,3,5,7,9,11,12], [1,2,3,4,5,10,12]))
-
-
+console.log(mergeArrays1([1, 3, 5, 7, 9, 11, 12], [1, 2, 3, 4, 5, 10, 12]))
